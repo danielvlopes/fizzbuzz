53cf124d4acf967b1a4cbb394f022a5aabc0a84b
João Vitor Lacerda Guimarães <joao.vitor@gmail.com>
2010-08-30 11:24:38 -0300

    dojo 20100828

--- fizzbuzz/test/test_dojo.rb
+++ fizzbuzz/test/test_dojo.rb
@@ -4,6 +4,30 @@ require 'minitest/unit'
 require 'minitest/spec'
 require 'minitest/autorun'
 
-describe Dojo do
+describe Dojo do                
+ before(:each) do
+   @dojo = Dojo.new
+ end 
+ it 'should receive a number' do
+   assert_equal 1, @dojo.fizzbuzz(1)
+ end
+  
+ it 'dever retornar fizz se o parametro for multiplo de 3' do
+   assert_equal 'fizz', @dojo.fizzbuzz(3)
+ end 
+ 
+ it 'dever retornar buzz se o parametro for multiplo de 5' do
+   assert_equal 'buzz', @dojo.fizzbuzz(5)
+ end 
+ 
+ it 'dever retornar fizzbuzz se o parametro for multiplo de 3 e 5' do
+   assert_equal 'fizzbuzz', @dojo.fizzbuzz(15)
+ end  
+ 
+ it 'deve retornar a seguinte expressao -> 1 2 fizz 4 buzz ...' do
+   expected = "1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz"
+   assert_equal expected, @dojo.sequence(15)
+ end
+end              
 
-end
+
